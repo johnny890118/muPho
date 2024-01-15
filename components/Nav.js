fd32e135e75c0a00b0e3e2017478cd1ddb538c59
Johnny <johnny0929560027@gmail.com>
2024-01-15 10:48:30 +0800

    muphoLogo style

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import mpLogo from "@/styles/mpLogo.drawio.png";
+import mpName from "@/public/muphoName.png";
 import Image from "next/image";
 
 const Nav = () => {
@@ -7,16 +8,12 @@ const Nav = () => {
     <div className="z-50 sticky top-0 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <nav className="h-[8vh]">
         <ul className="flex justify-between mx-4 sm:mx-8 items-center h-[8vh]">
-          <div className="logo px-4 flex items-center justify-center">
-            <Image
-              src={mpLogo}
-              className="h-[7vh] w-[7vh] muLogo"
-              alt=""
-            ></Image>
-            <h1 className="lobster-regular text-3xl font-bold leading-[6vh] ml-2">
-              MuPho
-            </h1>
-          </div>
+          <Link href="/">
+            <div className="logo px-4 flex items-center justify-center">
+              <Image src={mpLogo} className="h-[6vh] w-[6vh]" alt=""></Image>
+              <Image src={mpName} className="h-[6vh] w-auto" alt=""></Image>
+            </div>
+          </Link>
           <div className="bar flex">
             <li className="rounded-md m-1">
               <Link
