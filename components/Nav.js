6b050b2f0a13ada394a0b5ecb6c33e20640a80cb
Johnny <johnny0929560027@gmail.com>
2025-03-09 02:54:36 +0800

    refactor：code優化，樣式調整

--- components/Nav.js
+++ components/Nav.js
@@ -14,7 +14,7 @@ const Nav = ({ btnToInputHandler, isOverSearchArea }) => {
     <>
       <div
         className={`${
-          isOverSearchArea ? "bg-[#c3c0db]" : "bg-transparent"
+          isOverSearchArea ? "bg-[#c3c0db]/95" : "bg-transparent"
         } fixed top-0 z-10 w-full`}
       >
         <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 sm:py-3">
--- components/Picture.js
+++ components/Picture.js
@@ -1,16 +1,9 @@
-import Image from "next/image";
 import { MdOutlineFileDownload } from "react-icons/md";
 
 const Picture = ({ data }) => {
   return (
     <div className="relative mb-4 group">
-      <Image
-        src={data.src && data.src.large}
-        alt=""
-        className="rounded-xl sm:rounded-2xl"
-        width={400}
-        height={900}
-      />
+      <img src={data.src && data.src.large} alt="" className="rounded-xl sm:rounded-2xl" />
 
       <div className="absolute inset-0 flex flex-col justify-between bg-black/20  text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl sm:rounded-2xl p-4">
         <p className="text-gray-200">{data.photographer}</p>
